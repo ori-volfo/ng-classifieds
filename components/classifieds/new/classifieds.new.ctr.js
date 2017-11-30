ed30951f481ef9576f315988b7dff8a4543d4912
ori-volfo <ori.volfo@gmail.com>
2017-11-30 12:19:13 +0200

    advanced routing using watchers

--- components/classifieds/new/classifieds.new.ctr.js
+++ components/classifieds/new/classifieds.new.ctr.js
@@ -2,11 +2,25 @@
    "use strict";
     angular
         .module('ngClassifieds')
-        .controller('newClassifiedsCtrl',function($mdSidenav, $timeout, $mdDialog, classifiedsFactory){
+        .controller('newClassifiedsCtrl',function($scope, $mdSidenav, $state, $timeout, $mdDialog, classifiedsFactory){
             var vm = this;
+            vm.closeSidebar = closeSidebar;
 
             $timeout(function(){
                 $mdSidenav('left').open();
             });
+
+            $scope.$watch('vm.sidenavOpen',function(sidenav){
+                if(sidenav === false){
+                    $mdSidenav('left').close()
+                        .then(function(){
+                            $state.go('classifieds');
+                        });
+                }
+            });
+
+            function closeSidebar(){
+                vm.sidenavOpen = false;
+            }
         })
 })();
